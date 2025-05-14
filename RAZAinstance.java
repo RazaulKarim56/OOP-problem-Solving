ef4f576fd894506ceb55037dac088041033f778c
Md Razaul Karim <riyadhvai56@gmail.com>
2025-05-14 21:49:19 +0600

    Add files via upload

--- RAZAinstance.java
+++ RAZAinstance.java
@@ -1,11 +1,17 @@
 /*instance block*/
 public class RAZAinstance {
-    int a; double b;String c; 
+    int a; double b;String c;
+    void show(){
+      a=90;b=90.00;c="may Allah help me";
+      System.out.println(a+" "+b+" "+c);
+    } 
  RAZAinstance(){
     a=40;b=40;c="learn coding";
+    System.out.println(a+" "+c);
  }  
  {
     a=100;b=200;c="Razaul karim";
     System.out.println(a+" "+b+" "+c);
+    System.out.println();
  } 
 }
---
+++ RazaInstanceMain.java
@@ -0,0 +1,7 @@
+public class RazaInstanceMain {
+    public static void main(String[] args) {
+        
+        RAZAinstance ref=new RAZAinstance();
+       ref.show();
+    }
+}
